11358b83e228f94cf97cee532367ec0eab82d591
dpxrk <danielpark0503@gmail.com>
2024-06-03 14:33:18 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Navbar, Hero } from "./components";
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
+        </div>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
