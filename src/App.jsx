8ed9792aefbb53d95d782573723b747311cd4484
dpxrk <danielpark0503@gmail.com>
2024-06-03 18:20:20 -0400

    removed the stars because it broke the first canvas. not enough GPU/memory leak

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
 
           <div className="relative z-0">
             <Contact />
-            <StarsCanvas />
+            {/* <StarsCanvas /> */}
           </div>
         </div>
       </BrowserRouter>
