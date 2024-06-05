c951531077afd54d0d46b3fabc6a03ae6e42d9f0
dpxrk <danielpark0503@gmail.com>
2024-06-05 10:52:32 -0400

    update[

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,6 +32,7 @@ const Contact = () => {
     setLoading(true);
 
     emailjs
+
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
