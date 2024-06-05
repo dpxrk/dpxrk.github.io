a2dd5165f06efa28d43f608298da7ee5cf916449
dpxrk <danielpark0503@gmail.com>
2024-06-05 11:01:49 -0400

    wasnt receiving email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,11 +19,13 @@ const Contact = () => {
 
   const handleChange = (e) => {
     const { target } = e;
-    const { name, value } = target;
+    const { name, nameValue } = target;
+    const { email, emailValue } = emailTarget;
 
     setForm({
       ...form,
-      [name]: value,
+      [name]: nameValue,
+      [email]: emailValue,
     });
   };
 
@@ -32,7 +34,6 @@ const Contact = () => {
     setLoading(true);
 
     emailjs
-
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
